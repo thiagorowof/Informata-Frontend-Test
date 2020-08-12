c45d12d38a37783afd52461fdcb89dca68eb6c35
Andreson Souza <andresonjazz@gmail.com>
2020-08-12 17:14:52 -0300

    feat: adicione as dependências do primeng ao projeto

---
+++ andreson-souza/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CardModule,
+    ButtonModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
