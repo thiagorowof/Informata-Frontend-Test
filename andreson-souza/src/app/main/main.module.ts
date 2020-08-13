009cba1267f7b0ac231d7e1b461d004b1e966655
Andreson Souza <andresonjazz@gmail.com>
2020-08-13 17:14:56 -0300

    feat: crie o componente para exibição de produtos

--- andreson-souza/src/app/main/main.module.ts
+++ andreson-souza/src/app/main/main.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { ProductsComponent } from './products/products.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductsComponent],
   imports: [
     CommonModule,
     MainRoutingModule
