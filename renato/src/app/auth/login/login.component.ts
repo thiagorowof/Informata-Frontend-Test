a61976c85ba8eea4e9c83bf1b9acd9d01180ec73
RenatoValentim <rvrenato28@gmail.com>
2020-08-13 12:09:27 -0300

    feat: adicione a pagina de login

---
+++ renato/src/app/auth/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+
+@Component({
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      cpf: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  login(): void {
+    const credentials = {
+      cpf: this.loginForm.get('cpf').value,
+      password: this.loginForm.get('password').value,
+    };
+
+    this.authService.login(credentials).subscribe(
+      (user) => {
+        this.router.navigate(['/products']);
+      },
+      (err) => {
+        alert('Login Error');
+      }
+    );
+  }
+}
