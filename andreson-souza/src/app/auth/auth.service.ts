880a72dff1eca5bc7d9d9ba155a878162c2b0f58
Andreson Souza <andresonjazz@gmail.com>
2020-08-13 18:18:00 -0300

    feat: crie um método para armazenar os dados do usuário no localstorage

--- andreson-souza/src/app/auth/auth.service.ts
+++ andreson-souza/src/app/auth/auth.service.ts
@@ -1,11 +1,39 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private userSubject: BehaviorSubject<User>;
+  public user: Observable<User>;
 
+  constructor(
+    private router: Router,
+    private http: HttpClient) {
 
-  constructor() { }
+    this.userSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('user')));
+    this.user = this.userSubject.asObservable();
+  }
+
+  public get userValue(): User {
+    return this.userSubject.value;
+  }
+
+  login(username, password) {
+    return this.http.post<User>(`${environment.apiUrl}/users/authenticate`, { username, password })
+      .pipe(map(user => {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.userSubject.next(user);
+        return user;
+      }));
+  }
+
 }
