8fef106c8d009603ac5c51792cef225d3a3d74e7
RenatoValentim <rvrenato28@gmail.com>
2020-08-13 13:02:36 -0300

    feat: adicione o component home

--- renato/src/app/main/layout/home/home.component.spec.ts
+++ renato/src/app/main/layout/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { LayoutComponent } from './layout.component';
+import { HomeComponent } from './home.component';
 
-describe('LayoutComponent', () => {
-  let component: LayoutComponent;
-  let fixture: ComponentFixture<LayoutComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LayoutComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- renato/src/app/main/main.module.ts
+++ renato/src/app/main/main.module.ts
@@ -6,10 +6,10 @@ import { TableModule } from 'primeng/table';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { RegisterProductsComponent } from './pages/register-products/register-products.component';
-import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './layout/home/home.component';
 
 @NgModule({
-  declarations: [ProductsComponent, RegisterProductsComponent, LayoutComponent, ],
+  declarations: [ProductsComponent, RegisterProductsComponent, HomeComponent],
   imports: [
     CommonModule,
     MainRoutingModule,
