dda6557628d0e73185ace8de3dfd146214953e44
Andreson Souza <andresonjazz@gmail.com>
2020-08-13 00:29:36 -0300

    feat: adicione uma rota do componente de login para o de registro

--- andreson-souza/src/app/auth/auth-routing.module.ts
+++ andreson-souza/src/app/auth/auth-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: 'auth/login', component: LoginComponent},
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
 ];
 
 @NgModule({
