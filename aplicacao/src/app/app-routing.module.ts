e1c7feeff8e831c7728d6a25fb91bc5775f8ff95
joaoveloso7 <59774054+joaoveloso7@users.noreply.github.com>
2020-08-14 18:06:19 -0300

    feat(register): Criada, estilizada e feita a rota da página de Cadastro.

--- aplicacao/src/app/app-routing.module.ts
+++ aplicacao/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RegisterComponent } from './register/register.component'
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
+  {
+    path: '', component: LoginComponent
+  },
+  {
+    path: 'register', component: RegisterComponent
+  }
+
 ];
 
 @NgModule({
