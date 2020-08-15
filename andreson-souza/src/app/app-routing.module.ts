fc38c6454baf7c71bde1bda7147d9f8c92c272d1
Andreson Souza <andresonjazz@gmail.com>
2020-08-14 23:36:26 -0300

    feat: adicione um rota para componente de produtos

--- andreson-souza/src/app/app-routing.module.ts
+++ andreson-souza/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from './shared/mock';
+import { ProductsComponent } from './products/products.component';
+
+const authModule = () => import('./auth/auth.module').then(x => x.AuthModule);
+
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/auth/login'}
+  { path: '', component: ProductsComponent, canActivate: [AuthGuard] },
+  { path: 'account', loadChildren: authModule },
+
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
