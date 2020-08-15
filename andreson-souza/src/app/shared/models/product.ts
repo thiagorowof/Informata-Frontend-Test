efb1e833db1fb2253a0cdc468ef4036b1300c150
Andreson Souza <andresonjazz@gmail.com>
2020-08-15 01:09:41 -0300

    feat: crie um model com os atributos de produto

---
+++ andreson-souza/src/app/shared/models/product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  id: number;
+  codigoErp: string;
+  apresentacao: string;
+  nomeComercial: string;
+  nomeComercialComApresentacao: string;
+  unidadeMedidaAnvisa: string;
+  codigoBarrasPrincipal: string;
+}
