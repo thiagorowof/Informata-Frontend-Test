9efae2723ff328fb42ca5d8c74701c145a2bea00
Andreson Souza <andresonjazz@gmail.com>
2020-08-15 13:34:14 -0300

    refactor: organize os imports no módulo principal

--- andreson-souza/src/app/app.module.ts
+++ andreson-souza/src/app/app.module.ts
@@ -1,28 +1,35 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { AuthModule } from './auth/auth.module';
 import { PrimengModule } from './primeng.module';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { fakeBackendProvider } from './shared/mock';
 import { JwtInterceptor, ErrorInterceptor } from './shared/mock';
-
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    CommonModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AuthModule,
     PrimengModule,
   ],
