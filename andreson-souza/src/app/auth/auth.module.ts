55f978f6c571682bc4be008fe7f934264ab8a5cc
Andreson Souza <andresonjazz@gmail.com>
2020-08-14 23:29:30 -0300

    refactor: organize o módulo de autenticação

--- andreson-souza/src/app/auth/auth.module.ts
+++ andreson-souza/src/app/auth/auth.module.ts
@@ -1,29 +1,26 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AuthRoutingModule } from './auth-routing.module';
+import { LayoutComponent } from './layout/layout.component';
 import { LoginComponent } from './login/login.component';
-import { PrimengModule } from '../primeng.module';
 import { RegisterComponent } from './register/register.component';
+import { PrimengModule } from '../primeng.module';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent],
-  imports: [
-    CommonModule,
-    BrowserAnimationsModule,
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AuthRoutingModule,
-    PrimengModule
-  ]
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        AuthRoutingModule,
+        PrimengModule,
+        FormsModule
+    ],
+    declarations: [
+        LayoutComponent,
+        LoginComponent,
+        RegisterComponent
+    ]
 })
 export class AuthModule { }
