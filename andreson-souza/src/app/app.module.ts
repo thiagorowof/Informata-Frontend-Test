0ee510db7ef92a4ef2ec015ca544f13db0728767
Andreson Souza <andresonjazz@gmail.com>
2020-08-15 15:54:07 -0300

    refactor: remova linhas em branco

--- andreson-souza/src/app/app.module.ts
+++ andreson-souza/src/app/app.module.ts
@@ -6,7 +6,6 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
