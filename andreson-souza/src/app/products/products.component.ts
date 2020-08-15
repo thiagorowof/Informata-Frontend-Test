29c9f015df3659d641b6bd73edcf7ae66d8e6afb
Andreson Souza <andresonjazz@gmail.com>
2020-08-15 15:47:16 -0300

    feat: adicione um filtro para fazer busca na tabela

--- andreson-souza/src/app/products/products.component.ts
+++ andreson-souza/src/app/products/products.component.ts
@@ -1,9 +1,7 @@
 import { ProductService } from './../shared/services/product.service';
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../shared/models/product';
-import { AuthService } from '../shared/services/auth.service';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-products',
@@ -14,23 +12,30 @@ export class ProductsComponent implements OnInit {
 
   products: Product[];
 
+  cols: any[];
+
   constructor(
-    private authService: AuthService,
     private productService: ProductService,
     private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.listar();
+    this.listProducts();
   }
 
-  listar() {
-    let a = this.http.get<Product[]>('https://us-central1-estudo-974fe.cloudfunctions.net/front_end')
-      .pipe(map(data => data)).subscribe(
-        restItems => {
-          console.log(restItems);
-          this.products = restItems;
-        }
-      );
+  listProducts() {
+    this.productService.getProducts().then(products => this.products = products);
+
+    this.cols = [
+      { field: 'id', header: 'ID' },
+      { field: 'codigoErp', header: 'Código ERP' },
+      { field: 'apresentacao', header: 'Apresentação' },
+      { field: 'nomeComercial', header: 'Nome Comercial' },
+      { field: 'nomeComercialComApresentacao', header: 'Nome Comercial com Apresentação' },
+      { field: 'unidadeMedidaAnvisa', header: 'Unidade Medida Anvisa' },
+      { field: 'codigoBarrasPrincipal', header: 'Código Barras Principal' },
+    ];
+
+
   }
 
 }
