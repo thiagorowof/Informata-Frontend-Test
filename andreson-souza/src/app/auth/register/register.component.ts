29635ab5b1eff7f2d5e1e56a1e2a56b193276b00
Andreson Souza <andresonjazz@gmail.com>
2020-08-14 23:27:08 -0300

    fix: corrija os campos de input

--- andreson-souza/src/app/auth/register/register.component.ts
+++ andreson-souza/src/app/auth/register/register.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
 
-@Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
-})
+import { AuthService } from '../../shared/services/auth.service';
+
+@Component({ templateUrl: 'register.component.html' })
 export class RegisterComponent implements OnInit {
+  form: FormGroup;
+  loading = false;
+  submitted = false;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService,
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  get f() { return this.form.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authService.register(this.form.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['../login'], { relativeTo: this.route });
+        },
+        error => {
+          console.error(error);
+        });
+  }
 }
