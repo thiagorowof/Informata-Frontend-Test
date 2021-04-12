b89f89ad94573acb69adc112c61dd9a846733a69
cleicykerolim <cleicykerolim@gmail.com>
2021-04-12 14:13:20 -0300

    feat(Component): criando a pasta de views para o cadastro component

--- my-project-informata/src/app/app.module.ts
+++ my-project-informata/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CadastroComponent } from './views/cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
